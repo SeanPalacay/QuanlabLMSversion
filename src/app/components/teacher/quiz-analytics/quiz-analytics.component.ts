767570fa95ed4bccf94c8b51a62697f28d65eacf
ken2 <kennethjhonaycardo@gmail.com>
2024-05-02 00:45:38 +0800

    analytics

--- src/app/components/teacher/quiz-analytics/quiz-analytics.component.ts
+++ src/app/components/teacher/quiz-analytics/quiz-analytics.component.ts
@@ -28,7 +28,7 @@ export class QuizAnalyticsComponent {
     { name: 't Delacruz', quiz1: 1, quiz2: 1 }
   ];
 
-  dropdownOptions: string[] = ['Quiz analytics ', 'Quiz analytics 2'];
+  dropdownOptions: string[] = ['Quiz analytics 1', 'Quiz analytics 2'];
   selectedOption: string = this.dropdownOptions[0]; // Default selection
 
   barChartData1: any[] = []; // Initialize with an empty array
